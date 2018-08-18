755cf3b03c14965fb92021364f32d0422b35b3a8
Maciej Smusz <maciejsmusz@gmail.com>
2018-08-18 13:37:05 -0600

    Adding files.

--- server.js
+++ server.js
@@ -2,25 +2,12 @@
  IMPORTS
  **************************************************************************/
 
-
-var express = require('express')
-var history = require('connect-history-api-fallback')
-var path = require('path')
-var serveStatic = require('serve-static')
-
-
-/**************************************************************************
- EXPRESS SERVER
- **************************************************************************/
-
-
-var app = express()
-
-app.use(history({verbose: true}))
-app.use(serveStatic(path.join(__dirname, '/dist')))
-
-var port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log('Server started at http://localhost:5000')
-})
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+console.log(__dirname);
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
