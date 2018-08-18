dd5366c98031400a37a2bae26680819bf4cbef9b
Maciej Smusz <maciejsmusz@gmail.com>
2018-08-18 14:20:35 -0600

    i hope this works

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-/**************************************************************************
- IMPORTS
- **************************************************************************/
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-console.log(__dirname);
-app.use(serveStatic(`${__dirname}/dist`));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const app = express()
+
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+const port = process.env.PORT || 5000
+app.listen(port)
+// Log to feedback that this is actually running
+console.log('Server started on port ' + port)
