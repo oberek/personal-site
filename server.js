d61ea37a9b55ee18fd06b6a5af0541b5388fb12a
Maciej Smusz <maciejsmusz@gmail.com>
2018-08-18 13:58:03 -0600

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 console.log(__dirname);
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(`${__dirname}/dist`));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
